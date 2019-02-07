6e8095f3d25c0775a6a1118b3548c60405cb294c
Tristan Alvey <Tristan.j.alvey-1@ou.edu>
2019-02-07 10:29:55 -0600

    initial commit

---
+++ src/Todo.java
@@ -0,0 +1,24 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-07
+OU ID_______________: 113290859
+OU EMAIL____________: tristan.j.alvey-1@ou.edu
+
+Tasks:
+1. Create Animal equals method
+2. Test getters(Animal)
+3. Test setters(Animal)
+4. Test Full Constructor(Animal)
+5. Create Equipment equals method
+6. Test getters(Equipment)
+7. Test setters(Equipment)
+8. Test constructor(Equipment)
+
+Estimated Time (minutes):
+1. 5
+2. XXx
+
+Actual Time (minutes):
+1. 3
+2. XXX
+*/}

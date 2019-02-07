1f8e794cca5d138f45aefef787f67b083fcb068e
Tristan Alvey <Tristan.j.alvey-1@ou.edu>
2019-02-07 13:22:13 -0600

    Implemented Equipment toStringTest()

--- src/EquipmentTest.java
+++ src/EquipmentTest.java
@@ -9,4 +9,12 @@ public class EquipmentTest{
 		Assert.assertEquals(eq.getTotalPrice(), 9.0);
 		Assert.assertEquals(eq.getDescription(), "bat-shaped throwing stars");
 	}
+	public void toStringTest() throws AssertException {
+		Equipment eq = new Equipment("Batwing/3,10.0,9.0,bat-shaped throwing stars");
+
+		String expected = "Name: Batwing, Number: 3, Weight: 10.00 lbs, Price: $9.00 - bat-shaped throwing stars";
+		String actual = eq.toString();
+		Assert.assertEquals(expected, actual);
+	}
+	
 }
